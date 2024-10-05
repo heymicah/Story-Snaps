f143e0c6ac8cc3bdaf7629adf5bbb7365630aca6
yiningliu1 <145304243+yiningliu1@users.noreply.github.com>
2024-10-05 16:46:58 -0400

    stashed changes

--- frontend/api/VisionApi.js
+++ frontend/api/VisionApi.js
@@ -1,4 +1,8 @@
-const generateStory = async (base64Image, endStory, prevStories = "") => {
+export const generateStory = async (
+  base64Image,
+  endStory,
+  prevStories = ""
+) => {
   try {
     const response = await fetch("http://localhost:5000/generate", {
       method: "POST",

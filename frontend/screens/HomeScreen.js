22852ab007e64b5538ce470767b502b18732971f
adrianp-7 <aipavila7@gmail.com>
2024-10-06 14:42:49 -0400

    testing text color

--- frontend/screens/HomeScreen.js
+++ frontend/screens/HomeScreen.js
@@ -86,7 +86,7 @@ const styles = StyleSheet.create({
   headerText: {
     fontSize: 40,
     fontFamily: "Baloo2_700Bold",
-    color: "#FFF",
+    color: "#080C0C",
   },
   scrollView: {
     flex: 1,
